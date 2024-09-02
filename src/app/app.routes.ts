48c1efe71aa08a9e192bfbe265f1b5153c9cf09c
moAllElite <mouhamedn347@gmail.com>
2024-09-02 13:27:31 +0000

    5.Order form added to the project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,20 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import {OrderComponent} from './order/order.component';
+
+
 export const routes: Routes = [
   {
-       path:'/home',component: HomeComponent,
+      path:'',redirectTo:'home',pathMatch:'full'
+  },
+  {
+    path:'home',component: HomeComponent,
+  },
+  {
+      path:'register',component:RegisterComponent,
+  },
+  {
+    path:'pre-order',component:OrderComponent
   }
 ];
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {AsyncPipe, NgIf} from "@angular/common";
+import {FormGroup, ReactiveFormsModule,  FormsModule,  FormControl, Validators} from "@angular/forms";
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [ ReactiveFormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+
+}
