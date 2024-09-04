466967f5aae85655a8304e72967077ec6a1a81f4
moAllElite <mouhamedn347@gmail.com>
2024-09-04 12:34:52 +0000

    17.Error message added while fields are loose focus & is empty

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,13 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {AsyncPipe, NgIf} from "@angular/common";
 import {FormGroup, ReactiveFormsModule,  FormsModule,  FormControl, Validators} from "@angular/forms";
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ ReactiveFormsModule],
+  imports: [  ReactiveFormsModule,
+    FormsModule,
+    NgIf,
+    AsyncPipe,
+    RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
-   
+export class RegisterComponent implements OnInit{
+
+  protected formGroup !:FormGroup ;
+ ngOnInit(): void {
+   this.formGroup =  new FormGroup({
+    first_name: new FormControl('' , [Validators.required  ]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    last_name: new FormControl('', [Validators.required]),
+    city:new FormControl('',[Validators.required]),
+    phone: new FormControl('',  [Validators.required ]),
+    state:new FormControl('',[Validators.required]),
+    company:new FormControl('',[Validators.required]),
+    raison_social:new FormControl('',[Validators.required]),
+    ninea:new FormControl('',[Validators.required])
+ });
+
+ }
+
+  /**
+   ** Getters pour récupérer les champs du formulaire
+   *Get all informations from the fields
+   * ==> first name , last name ,email , phone ,city, company, ninea
+  */
+  get first_name(){
+    return this.formGroup.get('first_name');
+  }
+
+  get last_name(){
+    return this.formGroup.get('last_name');
+  }
+
+  get email(){
+    return this.formGroup.get('email');
+  }
+
+  get city (){
+    return this.formGroup.get('city');
+  }
+
+  get state(){
+    return this.formGroup.get('state');
+  }
+
+  get phone(){
+    return this.formGroup.get('phone');
+  }
+
+  get company(){
+    return this.formGroup.get('company');
+  }
+
+  get ninea(){return this.formGroup.get('ninea');}
+
+  get raison_social(){return this.formGroup.get('raison_social');}
 }
